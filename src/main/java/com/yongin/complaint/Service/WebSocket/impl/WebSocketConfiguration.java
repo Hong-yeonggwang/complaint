7561a4dc80a8e2ff7405da679646f1d1e68f4da5
DH Jeong <wjdehgns97@gmail.com>
2023-09-30 14:19:20 +0900

    WebSocket 작업중2

--- src/main/java/com/yongin/complaint/Service/WebSocket/impl/WebSocketConfiguration.java
+++ src/main/java/com/yongin/complaint/Service/WebSocket/impl/WebSocketConfiguration.java
@@ -1,5 +1,6 @@
 package com.yongin.complaint.Service.WebSocket.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
@@ -7,18 +8,19 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-import java.util.logging.SocketHandler;
-
 @Configuration
 @EnableWebSocket // 웹 소켓 관련 설정을 자동으로
 public class WebSocketConfiguration implements WebSocketConfigurer {
 
     @Override // 메소드 customize
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(signalingSocketHandler(), "/chat/:{roomId}") // 웹소켓 서버 entrypoint
+        registry.addHandler(webSocketHandler(), "/chat/{roomId}") // 웹소켓 서버 entrypoint
                 .setAllowedOrigins("*"); //CORS, 허용할 uri를 지정한다. (default는 same-origin만 허용)
     }
 
     @Bean
-    public WebSocketHandler signalingSocketHandler() { return new webSocketHandler(); }
+    public WebSocketHandler webSocketHandler() { return new MyWebSocketHandler(); }
+
+//    @Autowired
+//    MyWebSocketHandler myWebSocketHandler;
 }
