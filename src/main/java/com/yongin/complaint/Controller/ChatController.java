d66eeefe2c23fef02f79490ed2482722d5d3525e
DH Jeong <wjdehgns97@gmail.com>
2023-10-09 17:07:56 +0900

    채팅6 - 채팅방 생성시 Id 중복 검사

--- src/main/java/com/yongin/complaint/Controller/ChatController.java
+++ src/main/java/com/yongin/complaint/Controller/ChatController.java
@@ -21,6 +21,7 @@ import java.util.List;
 @RestController
 public class ChatController {
     List<ChatRoomInfoDTO> roomList = new ArrayList<ChatRoomInfoDTO>();
+    List<ChatRoomInfoDTO> myRoomList = new ArrayList<ChatRoomInfoDTO>();
     SecureRandom random  = new SecureRandom(); // 채팅방 고유 ID를 생성하기 위한 랜덤 객체
 
     /**
@@ -29,8 +30,7 @@ public class ChatController {
      * @return
      */
     @PostMapping(value = "/createChatRoom")
-    public boolean createRoom(@RequestBody JSONObject params){
-        boolean createSuccess = false;
+    public ChatRoomInfoDTO createRoom(@RequestBody JSONObject params){
         System.out.println("불럿냐?");
         ChatRoomInfoDTO newChatRoomInfoDTO = new ChatRoomInfoDTO();
 
@@ -38,46 +38,55 @@ public class ChatController {
 
         String roomName = (String) params.get("chatRoomName");
 //        int maxUsers = (Integer) params.get("maxUsers");
-//        System.out.println(maxUsers);
+//        String userId = (String) params.get("userId");
+
+        boolean checkNewChatRoomId = false;
 
         if(roomName != null && !roomName.trim().equals("")) {
-            System.out.println("이름있냐?");
-
             String uniqueId = new BigInteger(130, random).toString(32);
+
+            if(roomList.size() == 0) checkNewChatRoomId = true;
+
+            while(!checkNewChatRoomId){
+                for(int i = 0; i < roomList.size(); ++i){
+                    // Id가 중복이면 새로 만들고 처음부터 다시 검사
+                    if(uniqueId.equals(roomList.get(i).getChatRoomId())){
+                        uniqueId = new BigInteger(130, random).toString(32);
+                        i = 0;
+                    }
+                    if(i+1 == roomList.size()){
+                        checkNewChatRoomId = true;
+                    }
+                }
+            }
 
             newChatRoomInfoDTO.setChatRoomId(uniqueId);
             newChatRoomInfoDTO.setChatRoomName(roomName);
+//            newChatRoomInfoDTO.setChatRoomName(userId); // 방 만든 사람
             newChatRoomInfoDTO.setChatRoomCreatedDate(LocalDateTime.now());
             newChatRoomInfoDTO.setChatRoomLimited(9); // 방생성 잘 되면 maxUsers 로 받을 예정
 
             roomList.add(newChatRoomInfoDTO);
-            createSuccess = true;
         }
 
-        System.out.println("다 만들었냐? : " + roomList.size());
-
-        return createSuccess;
+        return newChatRoomInfoDTO;
     }
 
     /**
      * 방 정보가져오기
-//     * @param params
      * @return
      */
     @PostMapping(value = "/getChatRoomList")
     public List<ChatRoomInfoDTO> getRoomList(){
-//        @RequestBody HashMap<Object, Object> params
         return roomList;
     }
 
-//    private static JSONObject jsonToObjectParser(String jsonStr) {
-//        JSONParser parser = new JSONParser();
-//        JSONObject obj = null;
-//        try {
-//            obj = (JSONObject)parser.parse(jsonStr);
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//        return obj;
+    /**
+     * 내 방 정보가져오기
+     * @return
+     */
+//    @PostMapping(value = "/getMyChatRoomList")
+//    public List<ChatRoomInfoDTO> getRoomList(String userId){
+//        return myRoomList;
 //    }
 }
